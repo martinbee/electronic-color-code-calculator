ecf037522b6a033b16681abc3adefe490025e349
Martin Bee <martbee88@gmail.com>
2018-01-07 10:02:40 -0500

    feat: add linting

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  "extends": "airbnb",
+  "env": {
+    "browser": true,
+    "node": true,
+  },
+  "rules": {
+    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+    "react/require-default-props": 0,
+    "spaced-comment": 0,
+    "no-console": 0
+  },
+  "plugins": [
+    "react",
+    "jsx-a11y",
+    "import"
+  ],
+  "parser": "babel-eslint",
+};
